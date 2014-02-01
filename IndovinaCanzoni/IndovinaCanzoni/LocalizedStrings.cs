f28fde4254b2922d5592620aa384507ff7bf8f38
ar <rotandrea@gmail.com>
2014-02-01 15:02:24 +0100

    primo commit

---
+++ IndovinaCanzoni/IndovinaCanzoni/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using IndovinaCanzoni.Resources;
+
+namespace IndovinaCanzoni
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
