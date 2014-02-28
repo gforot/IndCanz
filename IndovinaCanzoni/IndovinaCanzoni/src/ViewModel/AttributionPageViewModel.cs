83168c461c4c1fc9a5032ba752ced80b866f9e33
arota <arota@softeam-tsa.it>
2014-02-28 16:00:29 +0100

    Indirizzo Nokia Mix Radio portato in costante in AttributionPageViewModel

--- IndovinaCanzoni/IndovinaCanzoni/src/ViewModel/AttributionPageViewModel.cs
+++ IndovinaCanzoni/IndovinaCanzoni/src/ViewModel/AttributionPageViewModel.cs
@@ -6,6 +6,8 @@ namespace IndovinaCanzoni.ViewModel
 {
     public class AttributionPageViewModel : ViewModelCommon
     {
+        public const string NokiaMixRadioAddress = "http://www.windowsphone.com/it-it/store/app/nokia-mixradio/f5874252-1f04-4c3f-a335-4fa3b7b85329";
+
         #region Commands
 
         public RelayCommand ExitCommand { get; private set; }
